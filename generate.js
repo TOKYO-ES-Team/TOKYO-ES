91ad9562ed5905309f086ea8955b97a8c5613cd2
HyunJae Lee <yuuki_hj@naver.com>
2025-05-01 20:07:46 +0900

    Added airport.json Edited generate.js

--- generate.js
+++ generate.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 const licenseText = require('./licenses.js');
 const defineText = require('./define.js');
+const raw = fs.readFileSync('airports.json');
+const airports = JSON.parse(raw);
 
-const fullText = licenseText + defineText;
+const airportText = airports.map(airport => {
+    return `${airport.ICAO} ${airport.FREQ} ${airport.LAT} ${airport.LOT} ${airport.AIRSPACE}`;
+}).join('\n');
+
+const fullText = licenseText + defineText + "\n[AIRPORT]\n" + airportText;
 
 fs.writeFileSync('TEST.sct', fullText, 'utf8');
 console.log('TEST.sct file is generated successfully!');
