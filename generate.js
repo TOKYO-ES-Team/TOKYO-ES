7f50cfb220dca9620d2ea86f87a9dff671e0a8b8
HyunJae Lee <yuuki_hj@naver.com>
2025-05-01 23:31:54 +0900

    Changed sort of RUNWAY, generate.js code

--- generate.js
+++ generate.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 const licenseText = require('./licenses.js');
 const defineText = require('./define.js');
-const raw = fs.readFileSync('./database/airports.json');
-const airports = JSON.parse(raw);
+const aptraw = fs.readFileSync('./airports.json');
+const rwyraw = fs.readFileSync('./runways.json');
+const airports = JSON.parse(aptraw);
+const runways = JSON.parse(rwyraw);
 
 const airportText = airports.map(airport => {
     return `${airport.ICAO} ${airport.FREQ} ${airport.LAT} ${airport.LOT} ${airport.AIRSPACE} ;- ${airport.NAME}`;
 }).join('\n');
 
-const fullText = licenseText + defineText + "\n[AIRPORT]\n" + airportText;
+const runwayText = runways
+    .sort((a, b) => a.ICAO.localeCompare(b.ICAO))
+    .map(runway => {
+        return `${runway.STARTRWY} ${runway.ENDRWY} ${runway.STARTHDG} ${runway.ENDHDG} ${runway.STARTLATON} ${runway.ENDLATON} ;- ${runway.ICAO}`;
+    })
+    .join('\n');
+
+
+const fullText = licenseText + defineText + "\n\n[AIRPORT]\n" + airportText + "\n\n[RUNWAY]\n" + runwayText;
 
 fs.writeFileSync('TEST.sct', fullText, 'utf8');
 console.log('TEST.sct file is generated successfully!');
